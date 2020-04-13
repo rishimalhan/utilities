428bac7ce0443795b7a2eaecb0de9d513cb40edd
rishimalhan <rmalhan0112@gmail.com>
2020-04-12 17:50:13 -0700

    timer functionality add which times different parts of code using methods start, elapsed, reset

---
+++ examples/timer.cpp
@@ -0,0 +1,28 @@
+#include <timer.hpp>
+
+int main(){
+    timer timer;
+
+    timer.start()
+
+    /// Lines of Code 
+
+    timer.elapsed(); // returns double value.
+
+    /// Lines of Code 
+
+    timer.elapsed(); // returns double value.
+
+    /// Lines of Code 
+
+    timer.reset();
+
+    /// Lines of Code 
+
+    timer.elapsed(); // returns double value.
+
+    // OPTIONAL
+    timer.end();
+
+    return 0;
+}
